95536b14b3f2d3a5f63046e540fde798f70b93c7
mmazzarolo <mazzarolomatteo@gmail.com>
2019-04-22 15:51:39 +0200

    Fixed missing react-hooks plugin

--- plugins.js
+++ plugins.js
@@ -5,5 +5,6 @@ module.exports = [
   { name: "eslint-plugin-flowtype", rulePrefix: "flowtype" },
   { name: "eslint-plugin-jsx-a11y", rulePrefix: "jsx-a11y" },
   { name: "eslint-plugin-react", rulePrefix: "react" },
+  { name: "eslint-plugin-react-hooks", rulePrefix: "react-hooks" },
   { name: "@typescript-eslint/eslint-plugin", rulePrefix: "@typescript-eslint" }
 ];
