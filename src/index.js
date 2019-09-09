aafefd1dbe26db7ab9de7ebb4a6eadef1cafec46
mmazzarolo <mazzarolomatteo@gmail.com>
2019-09-09 10:37:24 +0200

    Fix missing TS

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ const rules = {};
 
 // Requires and sets the rules of the eslint-plugins used by create-react-app.
 plugins.forEach(plugin => {
-  const pluginModule = require(plugin.name);
-  Object.keys(pluginModule.rules).forEach(ruleName => {
-    rules[`${plugin.rulePrefix}/${ruleName}`] = pluginModule.rules[ruleName];
-  });
-});
+  try {
+    const pluginModule = require(plugin.name)
+    Object.keys(pluginModule.rules).forEach(ruleName => {
+      rules[`${plugin.rulePrefix}/${ruleName}`] = pluginModule.rules[ruleName]
+    })
+  } catch (err) {
+    // If the user doesn't have typescript installed skip the @typescript-eslint
+    // rules setup. The other JS rules will still work correctly.
+    const isTypescriptMissing =
+      err.message === `Cannot find module 'typescript'` &&
+      plugin.rulePrefix === "@typescript-eslint"
+    if (!isTypescriptMissing) {
+      throw err
+    }
+  }
+})
 
 module.exports = {
   configs: {
